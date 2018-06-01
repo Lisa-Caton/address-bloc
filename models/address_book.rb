59a533552711cc3a33dae0b836db908521872e0c
Lisa Caton <lisacaton01@gmail.com>
2018-06-01 11:19:24 -0400

    delete 3 fields success

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,33 +1,47 @@
 #we tell Ruby to load the library named entry.rb relative to  address_book.rb's file path using require_relative
 require_relative 'entry'
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
-    def initialize
-      @entries = []
-    end
+  def initialize
+    @entries = []
+  end
 
 
-    def add_entry(name, phone_number, email)
-      #we create a variable to store the insertion "index"
-      index = 0
-      entries.each do |entry|
-        #we compare "name" with the name of the current "entry".
-        #If "name" lexicographically proceeds "entry.name", we've found the "index" to insert at.
-        #Otherwise we increment index and continue comparing with the other entries.
-        if name < entry.name
-          break
-        end
-        index +=1
+  def add_entry(name, phone_number, email)
+    #we create a variable to store the insertion "index"
+    index = 0
+    @entries.each do |entry|
+      #we compare "name" with the name of the current "entry".
+      #If "name" lexicographically proceeds "entry.name", we've found the "index" to insert at.
+      #Otherwise we increment index and continue comparing with the other entries.
+      if name < entry.name
+        break
       end
+      index +=1
+    end
+    #we insert a new entry into "entries" using the calculated index
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
 
-      #we insert a new entry into "entries" using the calculated index
-      entries.insert(index, Entry.new(name, phone_number, email))
+
+
+  #We want to iterate over every address in the address book
+  #if we find an entry whose name, phone_number, and email address,
+  #match the entry that we've passed in, then we want to delete that enty!
+  def remove_entry(name, phone_number, email)
+    #create a variable to hold the entries that we want deleted
+    delete_entry = nil
+    #iterate over, then find matching!
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+      delete_entry = entry
+      #we will assign that entry to the entry thats going to be deleted
+      end
     end
 
-    def delete_name(name)
-      entries.delete_if {|name| name === entry.name}
-    end
-
+    #at the end of the method we will delete the entry, from our entries array
+    @entries.delete(delete_entry)
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,12 +37,47 @@ RSpec.describe AddressBook do
     end
   end
 
-  describe "#delete_name" do
-    it "removes name from the entry" do
+  describe "#remove_entry" do
+    it "removes an entry using an name, phone_number, and email address" do
+      #create a new instance
       book = AddressBook.new
-      book.delete_name('Ada Lovelace')
+      #add a single new entry into the address book using add_entry method, passing in the 3 values
+      book.add_entry("Lee Smith", "411", "smith@gmail.com")
+      
+      #We want to add a second entry so that we have an entry to look for to delete
+      #keep track of name, phone_number, and email address,
+      #so we can pass those into the remove entry method
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+      #add this into our address book using the add entry method
+      #passing in the 3 variables
 
-      expect(book.entries.size).to eq(0)
+
+      #Expect:
+      # 1. book is the new instance of an entry,
+      # 2. entries is the array,
+      # 3. size (alias for length) returns the # of elements in self*.
+      expect(book.entries.size).to eq(2)
+      # #1-expect to see that the entry size is 2, bc we have 2 entries in our address book
+      # #2-expect to see 1 entry left in our book
+      # #3-Final-expect looking at the item in our entry, and making sure that its name is the new name
+      #this confirms the correct entry has been removed
+
+      #then remove an entry, passing in the 3 variables
+      #we will remove "Ada Lovelace"
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+
+      #This confirms the correct entry has been removed
+      expect(book.entries.first.name).to eq("Lee Smith")
+      #Expect:
+      # 1. book is the new instance of an entry,
+      # 2. entries is the array,
+      # 3. returns the first element of the array
+      # 4. to equal, the new 
+
     end
   end
 end
